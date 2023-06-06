63fb20ce1540f89a092da916f5cba1099f26b5cb
Make993 <113847820+Make993@users.noreply.github.com>
2023-06-06 10:55:44 +0300

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+    <img src="https://www.bentleymotors.com/content/dam/bentley/Master/Models/derivative-strategy-/continental-gt/continental-gt-base/24-my/config%2034%20Continental%20GT%20moonbeam%20850x479.png/_jcr_content/renditions/original.image_file.850.478.file/config%2034%20Continental%20GT%20moonbeam%20850x479.png" class="border-rounded pt-2">
+</div>
+<div class="col-9">
+    <h2>Continental Cuisine <style>TextDecoration:Underlined;</style></h2>
+    <p>The Continental GT captures the essence of the Continental GT range. The quintessential luxury grand tourer, it is a true design classic, showcasing the very best of British automotive craft. There is no more accomplished combination of high performance with the practicality you need to truly enjoy long journeys.<br>Its twin-turbocharged 4.0 litre engine has the power to deliver exhilarating performance the moment you put your foot down, accompanied by the much-loved exhaust note that only a V8 engine can provide. With four driving modes ranging from Comfort to Sport – and the facility to configure your own settings – the Continental GT guarantees a gratifying drive.</p>
+    </div>
+@endsection
